68fc7d5d1110a38a8a0d15c3504d17d38ec8255e
yusuf ridwan <horiyorrmi72@gmail.com>
2026-07-31 11:18:07 +0100

    chores:file tracing

--- client/finbank/next.config.ts
+++ client/finbank/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: __dirname,
     allowedDevOrigins: ['192.168.0.113'],
 };
 
